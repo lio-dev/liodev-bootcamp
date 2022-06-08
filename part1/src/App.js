5989969424afc5326b5c8f6c938ba3da2468fb7f
leonel lizarraga <leonel.lizarraga@brightsoft.io>
2022-06-08 14:18:23 -0400

    first commit

---
+++ part1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import Mensaje from './Mensaje.js'
+const App=()=> {
+  return(
+    <div className="App">
+      <Mensaje massage = "hola estamos"/>
+      <Mensaje massage = "trabajando en un curso"/>
+      <Mensaje massage = "de react"/>
+    </div>
+  )
+}
+
+export default App;
